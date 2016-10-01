699666bbf2da0a2aefedd427a462c96b159f03fb
Olly <olly.george@gmail.com>
2016-10-01 14:44:25 +0100

    Tested vector forward

--- driverTests.py
+++ driverTests.py
@@ -7,13 +7,12 @@ class DriveVector:
         self.y = y
         magnitude = (x**2+y**2)**0.5
         other = magnitude * (2-(abs(x)+1))/2
-        sys.stdout.write(str(magnitude))
         if x < 0:
-            self.lw = magnitude
-            self.rw = other
-        else:
-            self.lw = other
             self.rw = magnitude
+            self.lw = other
+        else:
+            self.rw = other
+            self.lw = magnitude
 class DriveVectorTests(unittest.TestCase):
     def test_x0_y0_left_wheel_0(self):
         vec = DriveVector(0,0)
@@ -23,7 +22,15 @@ class DriveVectorTests(unittest.TestCase):
         self.assertEqual(vec.rw, 0)
     def test_x_minus_1_y0_left_wheel_1_right_wheel_0(self):
         vec = DriveVector(-1, 0)
+        self.assertEqual(vec.rw, 1)
+        self.assertEqual(vec.lw, 0)
+    def test_x_1_y_0_left_wheel_0_right_wheel_1(self):
+        vec = DriveVector(1,0)
         self.assertEqual(vec.rw, 0)
         self.assertEqual(vec.lw, 1)
+    def test_turn_left(self):
+        vec = DriveVector(0.7071, 0.7071)
+        self.assertAlmostEqual(vec.rw, ((1-0.7071)/2), 4)
+        self.assertAlmostEqual(vec.lw, 1, 4)
 if __name__ == '__main__':
     unittest.main()
