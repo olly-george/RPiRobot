789f5c03367c924c8f9214887698796285f4fac8
Olly <olly.george@gmail.com>
2017-04-11 20:48:13 +0100

    Added second wheel to test

--- scratch/testWheels.py
+++ scratch/testWheels.py
@@ -6,11 +6,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+
 GPIO.output(22, 1)
 GPIO.output(23, 0)
+GPIO.output(17, 1)
+GPIO.output(18, 0)
 
 time.sleep(2)
 
 GPIO.output(22, 0)
 GPIO.output(23, 0)
+GPIO.output(17, 0)
+GPIO.output(18, 0)
 
